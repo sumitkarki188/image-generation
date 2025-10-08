aa0960e9e7068c801c4f1ef765169e37f5d3b852
Sumit Singh Karki <sumitkarki1114@gmail.com>
2025-10-08 19:12:13 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
-})
+  base: '/',
+});
