3ba43bfc3259b873e0557457f6fa96dd2e54fd01
Sumit Singh Karki <sumitkarki1114@gmail.com>
2025-10-09 11:26:04 +0530

    Update BuyCredit.jsx

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -83,7 +83,7 @@ const BuyCredit = () => {
             <p className="text-sm">{item.desc}</p>
             <p className="">
               {" "}
-              <span className="text-3xl font-medium">${item.price}</span> /{" "}
+              <span className="text-3xl font-medium">₹{item.price}</span> /{" "}
               {item.credits} credits
             </p>
             <button
